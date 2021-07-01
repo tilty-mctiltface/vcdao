8af63d4b2795ed42d401eb18c3898f8823d646ea
sshezi <sboneloshezi007@gmail.com>
2021-07-01 22:59:32 +0200

    Style chart component

--- src/app/investment-chart/investment-chart.component.ts
+++ src/app/investment-chart/investment-chart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {data} from "./mock-data";
 
 @Component({
   selector: 'app-investment-chart',
@@ -6,7 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./investment-chart.component.scss']
 })
 export class InvestmentChartComponent implements OnInit {
+  chartData: any[] = data
+  view: [number, number] = [700, 300];
 
+  // options
+  rangeFillOpacity: number = 1
+  legend: boolean = false;
+  showLabels: boolean = true;
+  animations: boolean = true;
+  yAxis: boolean = true;
+  xAxis: boolean = false;
+  showYAxisLabel: boolean = true;
+  showXAxisLabel: boolean = false;
+  xAxisLabel: string = 'Time';
+  yAxisLabel: string = 'Value';
+  timeline: boolean = true;
+
+  colorScheme = {
+    domain: ['#00FFF6']
+  };
   constructor() { }
 
   ngOnInit(): void {
