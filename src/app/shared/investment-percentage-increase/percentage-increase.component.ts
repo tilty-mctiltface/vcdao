1d344ff58e3d1b42d09e2caae45554587653e0e0
tstoderegger1 <86496776+tstoderegger1@users.noreply.github.com>
2021-07-01 21:53:21 +0200

    implemented percentage investment increase

---
+++ src/app/shared/investment-percentage-increase/percentage-increase.component.ts
@@ -0,0 +1,42 @@
+import { EventEmitter, OnChanges } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { TimeRange } from './time-ranges.enum';
+
+
+
+@Component({
+  selector: 'percentage-increase',
+  templateUrl: './percentage-increase.component.html',
+  styleUrls: ['./percentage-increase.component.scss']
+})
+export class InvestmentPercentageIncreaseComponent implements OnInit, OnChanges {
+
+  @Input() initialValue: number = 0;
+  @Input() currentValue: number = 0;
+
+  @Output() timeRangeChanged: EventEmitter<TimeRange> = new EventEmitter<TimeRange>();
+
+  percentage: number = 0;
+  TimeRange: typeof TimeRange = TimeRange;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.percentage = this.calculatePercentage();
+  }
+
+  ngOnChanges() {
+    this.percentage = this.calculatePercentage();
+  }
+
+  dateRangeChanged(timeRange: TimeRange): void {
+    this.timeRangeChanged.emit(timeRange);
+  }
+
+  private calculatePercentage(): number {
+    if (this.initialValue === 0) {
+      return 0;
+    }
+    return parseFloat(((this.currentValue - this.initialValue) / this.initialValue * 100).toFixed(2));
+  }
+}
---
+++ src/app/shared/investment-percentage-increase/time-ranges.enum.ts
@@ -0,0 +1,6 @@
+export enum TimeRange {
+    ONE_DAY,
+    ONE_WEEK,
+    ONE_MONTH,
+    ONE_YEAR
+}
