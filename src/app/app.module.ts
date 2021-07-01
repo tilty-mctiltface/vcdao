6ae11e5d9208042fb9493b926b4018d825a14ca2
Tobias Stoderegger <tstoderegger@gmail.com>
2021-07-01 21:43:16 +0200

    added missing imports in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,33 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {RouterModule} from "@angular/router";
-import {LandingPageComponent} from './landing-page/landing-page.component';
-import {PoolsComponent} from './pools/pools.component';
-import {DashboardComponent} from './statistics/dashboard.component';
-import {RewardsComponent} from './rewards/rewards.component';
-import {WebPlatformComponent} from './web-platform/web-platform.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatCardModule} from "@angular/material/card";
-import {MatButtonModule} from "@angular/material/button";
-import {MatProgressBarModule} from "@angular/material/progress-bar";
-import {MatGridListModule} from "@angular/material/grid-list";
-import {MatTableModule} from "@angular/material/table";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RouterModule } from "@angular/router";
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { PoolsComponent } from './pools/pools.component';
+import { DashboardComponent } from './statistics/dashboard.component';
+import { RewardsComponent } from './rewards/rewards.component';
+import { WebPlatformComponent } from './web-platform/web-platform.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatTableModule } from "@angular/material/table";
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { PoolDialogComponent } from './shared/dialogs/pool/pool-dialog.component';
-import {MatDialogModule} from "@angular/material/dialog";
+import { MatDialogModule } from "@angular/material/dialog";
 import { AdvancedComponent } from './statistics/advanced/advanced.component';
 import { SimpleComponent } from './statistics/simple/simple.component';
 import { ProComponent } from './statistics/pro/pro.component';
 import { SearchBarComponent } from './shared/search-bar/search-bar.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import {MatDividerModule} from "@angular/material/divider";
-import { PriceIncreaseComponent } from './price-increase/price-increase.component';
+import { MatDividerModule } from "@angular/material/divider";
+import { InvestmentChartComponent } from './investment-chart/investment-chart.component';
 import { InvestmentPercentageIncreaseComponent } from './investment-percentage-increase/investment-percentage-increase.component';
 import { InvestmentValueComponent } from './investment-value/investment-value.component';
-import { InvestmentChartComponent } from './investment-chart/investment-chart.component';
+
 
 @NgModule({
   declarations: [
@@ -43,10 +43,9 @@ import { InvestmentChartComponent } from './investment-chart/investment-chart.co
     ProComponent,
     SearchBarComponent,
     FooterComponent,
-    PriceIncreaseComponent,
+    InvestmentChartComponent,
     InvestmentPercentageIncreaseComponent,
     InvestmentValueComponent,
-    InvestmentChartComponent
   ],
   imports: [
     BrowserModule,
