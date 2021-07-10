4573784dedcc92b0aee8cff239d2548960788626
sshezi <sboneloshezi007@gmail.com>
2021-07-10 15:52:42 +0200

    Ticker

--- src/app/components/running-ticker/running-ticker.component.ts
+++ src/app/components/running-ticker/running-ticker.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {CoingeckoService} from "../../services/coingecko/coingecko.service";
+import {Token} from "../../dtos/Token";
 
 @Component({
   selector: 'app-running-ticker',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./running-ticker.component.scss']
 })
 export class RunningTickerComponent implements OnInit {
-
-  constructor() { }
+  private tokenNames = ['eos', 'velas', 'tezos', 'eleven-finance','terra-luna', 'tbcc-wallet']
+  public tokens: Token[] = []
+  constructor(private coinGecko: CoingeckoService) { }
 
   ngOnInit(): void {
+    this.coinGecko.getTokensById(this.tokenNames).subscribe(
+      (val) => {
+        this.tokens = val
+      }
+    )
   }
 
 }
