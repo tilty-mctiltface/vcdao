ad4ff934d6aadd4e9bca55439370cb5367a7a477
mikaeljohnson <mikaeljohnsoncrypto@gmail.com>
2021-07-13 14:47:14 -0400

    web3 additions for debugging

--- src/app/services/wallet.service.ts
+++ src/app/services/wallet.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import Web3 from "web3";
+import { vcs } from './abis/VCSToken'
 
 declare let window: any;
 
@@ -8,6 +9,7 @@ declare let window: any;
 })
 export class WalletService {
   web3: any
+  vcsToken: any
 
   async walletConnect() {
     if (window.ethereum) {
@@ -19,6 +21,14 @@ export class WalletService {
     } else {
       window.alert('Non-Ethereum browser detected. You Should consider using MetaMask!');
     }
+    // Load VCSToken
+      const VCSToken = new window.web3.eth.Contract(vcs, '0xE8D63599bE079022ac4d5CdE3A48aED0F5AD491E')
+      this.vcsToken = VCSToken 
+      let vcsTokenBalance = await this.vcsToken.methods.balanceOf('0xE8D63599bE079022ac4d5CdE3A48aED0F5AD491E').call()
+      this.vcsToken({ vcsTokenBalance: vcsTokenBalance.toString() })
+      console.log( this.vcsToken.vcsTokenBalance )
+    
+    
   }
 
   detectEthereumNetwork() {
